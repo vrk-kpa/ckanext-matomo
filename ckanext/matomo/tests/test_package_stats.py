021452b2e795115dca3afc9e56a0885ed018e694
Pasi Laakso <pasi.laakso@gofore.com>
2022-11-17 16:10:15 +0200

    AV-1851: Add some tests

---
+++ ckanext/matomo/tests/test_package_stats.py
@@ -0,0 +1,33 @@
+import pytest
+import ckan.tests.factories as factories
+from datetime import date, datetime, timedelta
+from ckanext.matomo.model import PackageStats
+from ckanext.matomo.commands import init_db
+import logging
+log = logging.getLogger(__name__)
+
+
+@pytest.mark.usefixtures("clean_db")
+def test_resource_update_downloads(app):
+    init_db()
+    package_id = '16364c67-251c-45dc-98d9-9e91105d1928'
+    stat_date = datetime.strptime('2022-11-10', '%Y-%m-%d')
+    PackageStats.update_downloads(package_id, stat_date, 3)
+    package_stats = PackageStats.get(package_id)
+    assert package_stats.__dict__.get('downloads') == 3
+    PackageStats.update_downloads(package_id, stat_date, 2)
+    package_stats = PackageStats.get(package_id)
+    assert package_stats.__dict__.get('downloads') == 2
+
+
+@pytest.mark.usefixtures("clean_db")
+def test_resource_update_visits(app):
+    init_db()
+    package_id = '16364c67-251c-45dc-98d9-9e91105d1928'
+    stat_date = datetime.strptime('2022-11-10', '%Y-%m-%d')
+    PackageStats.update_visits(package_id, stat_date, 3)
+    package_stats = PackageStats.get(package_id)
+    assert package_stats.__dict__.get('visits') == 3
+    PackageStats.update_visits(package_id, stat_date, 2)
+    package_stats = PackageStats.get(package_id)
+    assert package_stats.__dict__.get('visits') == 2
