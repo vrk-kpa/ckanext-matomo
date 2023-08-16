b21f6406bbfb7d1b58484e813c6a53d3ca780c83
Jari-Pekka Voutilainen <jari-pekka.voutilainen@gofore.com>
2023-08-16 11:59:04 +0300

    Add support for piwik script names

--- ckanext/matomo/helpers.py
+++ ckanext/matomo/helpers.py
@@ -12,7 +12,8 @@ def matomo_snippet():
     data = {
         "matomo_domain": config.get('ckanext.matomo.domain'),
         "matomo_script_domain": config.get('ckanext.matomo.script_domain', config.get('ckanext.matomo.domain')),
-        "matomo_site_id": config.get('ckanext.matomo.site_id')
+        "matomo_site_id": config.get('ckanext.matomo.site_id'),
+        "matomo_filename": config.get('ckanext.matomo.filename', "matomo")
     }
 
     return render_snippet("matomo/snippets/matomo.html", data)
